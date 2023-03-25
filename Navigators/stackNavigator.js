2ec0474c727fa572e2d2e16fa06938037d931d05
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2023-03-26 02:22:38 +0530

    added assets and stack navigator

---
+++ Navigators/stackNavigator.js
@@ -0,0 +1,46 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {IconButton} from 'native-base';
+import AttendanceList from '../Screens/AttendanceList';
+import GenerateQr from '../Screens/GenerateQr';
+import Login from '../Screens/Login';
+import Signup from '../Screens/Signup';
+
+const Stack = createStackNavigator();
+
+const MyStack = ({navigation}) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="GenerateQR"
+        component={GenerateQr}
+        options={{
+          headerTitle: 'Generate QR Code',
+        }}
+      />
+
+      <Stack.Screen
+        name="AttendanceList"
+        component={AttendanceList}
+        options={{
+          headerTitle: 'Attendance List',
+        }}></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
+
+export default MyStack;
