79e014607f38ff01efb2df050d33d3d9f06f6fda
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2023-03-26 18:36:26 +0530

    attendance list created

--- Screens/AttendanceList.js
+++ Screens/AttendanceList.js
@@ -1,17 +1,52 @@
-import {Button, Center, HStack, Text} from 'native-base';
+import {
+  Button,
+  Center,
+  Divider,
+  HStack,
+  ScrollView,
+  Spacer,
+  Spinner,
+  Text,
+  VStack,
+} from 'native-base';
 import {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
 import {useState} from 'react';
-import {collection, onSnapshot, query, where} from 'firebase/firestore';
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
 import {auth, db} from '../firebase-config';
 
 const AttendanceList = () => {
   const [date, setDate] = useState(new Date());
   const [collegeName, setCollegeName] = useState('');
+  const [attendanceData, setAttendanceData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState();
 
+  const compareDate = (d1, d2) => {
+    const date1 = new Date(d1);
+    const date2 = new Date(d2);
+    date1.toUTCString();
+    date2.toUTCString();
+    if (
+      date1.getUTCDate() === date2.getUTCDate() &&
+      date1.getUTCMonth() === date2.getUTCMonth() &&
+      date1.getUTCFullYear() === date2.getUTCFullYear()
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   const onChange = (event, selectedDate) => {
+    setAttendanceData([]);
+    setSelectedDate(selectedDate);
     const currentDate = selectedDate;
     setDate(currentDate);
-    console.log(currentDate.toUTCString());
+    // console.log(currentDate);
     const uid = auth?.currentUser.uid;
     const q = query(collection(db, 'collegeAdmins'), where('uid', '==', uid));
     onSnapshot(q, snapshot => {
@@ -20,18 +55,23 @@ const AttendanceList = () => {
         setCollegeName(data);
       });
     });
+    // fetchAttendance();
+  };
 
-    const q2 = query(
+  const fetchAttendance = () => {
+    setAttendanceData([]);
+
+    const q = query(
       collection(db, 'attendance'),
       where('collegeName', '==', collegeName),
+      orderBy('time', 'asc'),
     );
-    onSnapshot(q2, snapshot => {
+    onSnapshot(q, snapshot => {
       snapshot.docs.forEach(doc => {
-        if (doc.data().date === currentDate.toUTCString()) {
-          console.log('matched');
-        }
         const data = doc.data();
-        console.log(data);
+        if (compareDate(data.date, date)) {
+          setAttendanceData(prev => [...prev, data]);
+        }
       });
     });
   };
@@ -47,8 +87,56 @@ const AttendanceList = () => {
   return (
     <Center width={'100%'}>
       <Button onPress={showDatePicker} width={'90%'} marginTop={3}>
-        <Text style={{color: 'white'}}>Pick a Date</Text>
+        <Text style={{color: 'white'}}>
+          {selectedDate ? selectedDate.toDateString() : 'Select a Date'}
+        </Text>
       </Button>
+      <Button onPress={fetchAttendance} width={'90%'} marginTop={3}>
+        <Text style={{color: 'white'}}>Fetch Data</Text>
+      </Button>
+
+      {attendanceData.length > 0 ? (
+        <>
+          <HStack
+            width={'90%'}
+            justifyContent={'space-between'}
+            marginTop={3}
+            marginBottom={3}>
+            <Text width={'15%'} fontWeight={'bold'}>
+              S.No
+            </Text>
+            <Text width={'25%'} fontWeight={'bold'}>
+              Name
+            </Text>
+            <Text width={'25%'} fontWeight={'bold'}>
+              Enrollment No.
+            </Text>
+            <Text width={'25%'} fontWeight={'bold'}>
+              In Time
+            </Text>
+          </HStack>
+          <Divider thickness={2} bg={'black'} width={'90%'} />
+
+          {attendanceData?.map((item, index) => {
+            return (
+              <>
+                <HStack
+                  key={index}
+                  width={'90%'}
+                  justifyContent={'space-between'}
+                  marginTop={3}>
+                  <Text width={'15%'}>{index + 1}</Text>
+                  <Text width={'25%'}>{item?.name}</Text>
+                  <Text width={'25%'}>{item?.enrollmentno}</Text>
+                  <Text width={'25%'}>{item?.time}</Text>
+                </HStack>
+              </>
+            );
+          })}
+        </>
+      ) : (
+        <></>
+      )}
     </Center>
   );
 };
